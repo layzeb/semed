a00661686defdb40da53eb151d430851d81d4ce5
Layze Brandao <layzebrand@gmail.com>
2017-06-22 16:52:21 -0300

    Versão 3.0: tratamento de erros

--- Calculadora de Quinquenios.py
+++ Calculadora de Quinquenios.py
@@ -1,8 +1,6 @@
 from time import strftime
 
-# uso do metodo time.strftime para capturar o ano atual com 04 digitos
 ano_atual = int(strftime("%Y"))
-# inicialização do ano de admissao
 adm = total = 0
 nome = mat = ''
 
@@ -10,9 +8,8 @@ nome = mat = ''
 def menu():
     """Função que estabelece o menu inicial do programa."""
 
-    print("\t\tCALCULADORA DE QUINQUENIOS - LICENCA PREMIO v2.1.4 BETA\n\n")
-    print(
-        "Este programa foi desenvolvido para calcular quantos quinquenios de licenca premio o servidor da SEMED/Sao Luis tem direito.\n")
+    print("\t   CALCULADORA DE QUINQUÊNIOS - LICENÇA PRÊMIO v3.0 BETA\n\n")
+    print("Calcule quantos quinquênios de licenca prêmio o servidor da SEMED tem direito.\n")
     execucao()
     relatorio()
 
@@ -20,124 +17,105 @@ def menu():
 def execucao():
     """Função que solicita admissao do servidor e retorna a quantidade de quinquenios disponiveis para análise."""
 
-    # chamada das variaveis globais para serem usadas dentro da definição da função
     global ano_atual, adm, nome, mat
 
     nome = input("Nome do servidor: ")
-    mat = input("Matricula: ")
+    mat = input("Matrícula: ")
 
-    # solicita ano de admissao e armazena na variavel adm
     while True:
-        adm = int(input("\nDigite o ano de admissao com 04 digitos e aperte ENTER [ex: 1988]: \n"))
-        # checa se o ano de admissao é válido
-        if adm > ano_atual:
-            print("ERRO! Admissao posterior ao ano atual. Tente novamente.\n")
-            continue
-        else:
-            break
+        try:
+            adm = int(input("\nDigite o ano de admissão com 04 dígitos e aperte ENTER [ex: 1988]: \n"))
+            if len(str(adm)) != 4:
+                print('O ano precisa ter 04 dígitos. Exemplo: 2010 ')
+                continue
+            else:
+                if adm > ano_atual:
+                    print("Admissão posterior ao ano atual. Tente novamente.")
+                    continue
+                else:
+                    break
+        except ValueError:
+            print("Você usou um valor não-numérico. Tente novamente.")
 
-    # iteração sobre as hipoteses de análise
     while True:
-
-        # solicita se o servidor possui algum impedimento e armazena na variavel faltas_lic
-        faltas_lic = input("O servidor possui faltas injustificadas, licencas para tratar de interesse particular ou acompanhar familiares? \
-Digite 's' para 'SIM' ou 'n' para 'NAO' e aperte ENTER: \n")
-        faltas_lic = faltas_lic.lower()
-        # checa se o usuario optou por 'sim', executa a função com_imped e interrompe o laço
+        faltas_lic = input("O servidor possui faltas injustificadas, licenças para tratar de interesse particular ou acompanhar familiares? \
+Digite 's' para 'SIM' ou 'n' para 'NÃO' e aperte ENTER: \n").lower()
         if faltas_lic == "s":
             com_imped()
             break
-
-        # checa se o usuario optou por 'nao', executa a função sem_imped e interrompe o laço
         elif faltas_lic == "n":
             sem_imped()
             break
-
-        # reinicia o loop, caso a opção escolhida seja diferente das acima
         else:
-            print("Opcao invalida. Digite 's' para 'SIM' ou 'n' para 'NAO':\n")
+            print("Opção inválida. Digite 's' para 'SIM' ou 'n' para 'NÃO':\n")
             continue
 
 
 def sem_imped():
-    """Função que calcula os quinquenios disponiveis \
-para licenca de um servidor sem impedimentos (faltas injustificadas, licencas paratratar de interesse particular ou acompanhar familiares)."""
+    """Função que calcula os quinquenios disponiveis para licenca de um servidor sem impedimentos\
+(faltas injustificadas, licencas para tratar de interesse particular ou acompanhar familiares)."""
 
     global ano_atual, adm, total
 
-    # calcula a quantidade de quinquenios disponiveis
-    quinq = (ano_atual - adm) // 5
-
-    # recebe a quantidade de quinquenios tirados pelo servidor
-    lic_tirada = int(input(
-        "Digite quantos quinquenios o servidor ja tirou. Se o servidor nunca usufruiu de Licenca Premio, digite 0: \n"))
-
-    # checa se a quantidade de licencas tiradas é maior do que as disponiveis
-    if lic_tirada <= quinq:
-        # calcula o total disponivel e mostra ao usuario
-        total = quinq - lic_tirada
-        print("O servidor tem direito a {} quinquenio(s) = {} meses".format(total, total * 3))
-    else:
-        print("ERRO! O servidor ainda não completou {} quinquenios, tente novamente.".format(lic_tirada))
+    while True:
+        quinq = (ano_atual - adm) // 5
+        try:
+            lic_tirada = int(input(
+                "Digite quantos quinquênios o servidor já tirou. Se o servidor nunca usufruiu de Licença Prêmio, digite 0: \n"))
+            if lic_tirada <= quinq:
+                total = quinq - lic_tirada
+                print("O servidor tem direito a {} quinquenio(s) = {} meses".format(total, total * 3))
+            else:
+                print("O servidor ainda não completou {} quinquenios, tente novamente.".format(lic_tirada))
+            break
+        except ValueError:
+            print("Você usou um valor não-numérico. Tente novamente.")
 
 
 def com_imped():
     global ano_atual, adm, total
 
-    # cria uma lista que irá armazenar todos os anos analisados
     anos = []
-
-    # itera desde o ano de admissao até o atual, incluindo cada ano na lista anos
     for i in range(adm, ano_atual + 1):
         anos.append(i)
 
-    print("Digite o ano em que o servidor possui falta, licenca sem vencimento ou licenca para acompanhar familiares, UM POR VEZ, e aperte ENTER.\n\
-Caso a licenca seja maior que um ano, colocar todos os anos de afastamento, sempre UM POR VEZ e aperte ENTER.\n\
+    print("Digite o ano em que o servidor possui falta, licença sem vencimento ou licença para acompanhar familiares, UM POR VEZ, e aperte ENTER.\n\
+Caso a licença seja maior que um ano, colocar todos os anos de afastamento, sempre UM POR VEZ e aperte ENTER.\n\
 Para parar de adicionar anos, digite SAIR e aperte ENTER\n")
 
     while True:
-
-        # solicita um ano em que há impedimento
         ano_imp = input().lower()
-        # checa se o usuario digitou 'sair' e sai do loop
         if ano_imp == 'sair':
             break
         else:
-            # transforma o tipo de ano_imp em int
             ano_imp = int(ano_imp)
-
-            # checa se o usuario informou um ano válido
             if ano_imp < adm or ano_imp > ano_atual:
-                print("ERRO! Insira um ano valido")
+                print("Insira um ano válido!\n")
                 continue
 
-            # checa se o ano_imp está na lista anos
             if ano_imp in anos:
-                # se ano_imp estiver em anos, acha sua posição
                 pos = anos.index(ano_imp)
-                # remove o item da lista anos
                 del anos[pos]
             else:
-                # se o ano já tiver sido apagado, reinicia o loop
                 continue
 
-    # calcula a quantidade de anos presentes em anos[]
-    # subtrai-se 1 pq o ano inicial não é contado
     qt_anos = len(anos) - 1
     quinquenios = qt_anos // 5
-    qt_premio = int(input(
-        "Digite quantos quinquenios o servidor ja tirou. Se o servidor nunca usufruiu de Licenca Premio, digite 0 e aperte ENTER: \n"))
-
-    # checa se a quantidade de licencas tiradas é maior do que as disponiveis
-    if qt_premio <= quinquenios:
-        total = (quinquenios - qt_premio)
-        print("O servidor tem direito a {} quinquenio(s) = {} meses.".format(total, total * 3))
-    else:
-        print("ERRO! O servidor ainda não completou {} quinquenios, tente novamente.".format(qt_premio))
+    try:
+        qt_premio = int(input(
+            "Digite quantos quinquênios o servidor já tirou. Se o servidor nunca usufruiu de Licença Prêmio, digite 0 e aperte ENTER: \n"))
+        if qt_premio <= quinquenios:
+            total = (quinquenios - qt_premio)
+            print("O servidor tem direito a {} quinquênio(s) = {} meses.".format(total, total * 3))
+        else:
+            print("O servidor ainda não completou {} quinquênios, tente novamente.".format(qt_premio))
+    except ValueError:
+        print("Você usou um valor não-numérico. Tente novamente.\n")
 
 
 def relatorio():
-    """Função que irá gerar um relatorio da consulta realizada pronto para impressão."""
+    """Função que irá gerar um relatório da consulta realizada pronto para impressão."""
+
     global nome, mat, total
 
     arq_rel = open("relatorio.txt", 'w')
